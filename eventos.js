10c276c2b6ca6b38bfc69d2aeb3b1dbaa6a27109
sofiahubert <sofia.hubert.gonzalez@gmail.com>
2023-08-07 14:59:46 -0300

    java para pagina

---
+++ eventos.js
@@ -0,0 +1,9 @@
+// Obtener el botón por su ID
+const boton = document.getElementById("miBoton");
+// Obtener el div existente por su ID
+const nuevoDiv = document.getElementById("nuevoDiv");
+// Agregar el botón como contenido del div
+nuevoDiv.appendChild(boton);
+nuevoDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
